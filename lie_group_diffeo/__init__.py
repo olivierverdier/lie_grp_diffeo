6bb6f2b0f80132acca3ecf76ba8bd5829da8f6fc
Olivier Verdier <olivier.verdier@gmail.com>
2018-04-19 11:42:29 +0200

    remove imports of `__all__`

--- lie_group_diffeo/__init__.py
+++ lie_group_diffeo/__init__.py
@@ -1,20 +1,13 @@
 from __future__ import absolute_import
 
 
-__all__ = ()
-
 
 from .action import *
-__all__ += action.__all__
 
 from .lie_group import *
-__all__ += lie_group.__all__
 
 from .matrix_group import *
-__all__ += matrix_group.__all__
 
 from .diff_group import *
-__all__ += diff_group.__all__
 
 from .solver import *
-__all__ += solver.__all__
